3c6cfe5a3d5f3f5303a809dd029d74290b51add1
zraxon <zixux@hotmail.com>
2016-10-19 15:20:39 +0100

    Update food.py

--- food.py
+++ food.py
@@ -1,21 +1,23 @@
 import numpy as np
-from Direction import 
+from direction import Direction as di
 
 class Food:
        
-    def __init__(self, position, energy):
-        self.position = np.array(position)
-        self.energy = energy
-    
+    def __init__(self, position, energy, repThresh, energyMax): 
+       self.position = np.array(position)
+       self.energy = energy
+       self.repThresh = repThresh
+       self.energyMax = energyMax
+       
     def get_energy_value(self):
         return self.energy
-    
+       
     def grow(self):
         self.energy += 1
-    
-    def reproduce(self, new):
-        if self.energy >= 20:
-            spawnp = np.random.random_integers(4)
+              
+    def reproduce(self, new, delx, dely, food_list):
+        if self.energy >= self.repThresh:
+              if np.random.random_integers(4) = di.up
             
     def die(self):
         if self.energy <= 0:
