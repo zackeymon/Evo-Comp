8382ef1ccbe3fe6b5c058320b3a47fbe1df794ca
zraxon <zixux@hotmail.com>
2016-10-19 15:06:36 +0100

    Update food.py

--- food.py
+++ food.py
@@ -1,9 +1,20 @@
+import numpy as np
+
 class Food:
-
-    def __init__(self, x, y, energyVal):
-        self.x = x
-        self.y = y
-        self.energyVal = energyVal
-
+       
+    def __init__(self, position, energy):
+        self.position = np.array(position)
+        self.energy = energy
+    
     def get_energy_value(self):
-        return self.energyVal
+        return self.energy
+    
+    def grow(self):
+        self.energy += 1
+    
+    def reproduce(self, new):
+        if self.energy >= 20:
+            spawnp = np.random.random_integers(4)
+            
+    def die(self):
+        if self.energy <= 0:
