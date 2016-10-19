51c150ea2fc63741855af60f6a1d19c1a528d33a
zraxon <zixux@hotmail.com>
2016-10-19 15:33:36 +0100

    Update direction.py

--- direction.py
+++ direction.py
@@ -1,3 +1,4 @@
+import numpy as np
 from enum import Enum
 
 class Direction(Enum):
@@ -5,3 +6,17 @@ class Direction(Enum):
   down = 2
   left = 3
   right = 4
+
+  def random():
+  randomP = np.random.random_integers(4)
+  delx = 0
+  dely = 0
+    if randomP == Direction.up:
+      dely += 1
+    elif randomP == Direcion.down:
+      dely -= 1
+    elif randomP == Direction.left:
+      delx -= 1
+    elif randomP == Direction.right:
+      delx += 1
+      
