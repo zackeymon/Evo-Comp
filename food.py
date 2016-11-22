30b89189b52cd3a3c7fd68270580e3c5019bb78c
AoxerX <ant385@hotmail.co.uk>
2016-11-22 17:21:25 +0000

    Update food.py

--- food.py
+++ food.py
@@ -5,15 +5,16 @@ class Food(Organism):
     """
     A class for a food organism that simply grows and sustains life.
     """
-    def __init__(self, position, energy, reproduction_threshold, energy_max):
+    def __init__(self, position, energy, reproduction_threshold, energy_max, gene_val):
         """
         Food Initialisation
         :param position: The current position of the food in the world
         :param energy: The energy stored in the food
         :param reproduction_threshold: The energy value at which the food reproduces
         :param energy_max: The maximum energy the food can hold
+        :param gene_val: The gene paramter of the food
         """
-        Organism.__init__(self, position, energy, reproduction_threshold, energy_max)
+        Organism.__init__(self, position, energy, reproduction_threshold, energy_max, gene_val)
 
     def grow(self):
         if self.energy < self.energy_max:
