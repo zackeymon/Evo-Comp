f09a568554d3be59cf2134a3e2f5e188ba124a15
Zack <zixux@hotmail.com>
2016-11-22 10:14:18 +0000

    Removed lifetime as a parameter. Cleaned up the code

--- food.py
+++ food.py
@@ -1,21 +1,19 @@
 from organism import Organism
-from random import randint
 
 
 class Food(Organism):
     """
     A class for a food organism that simply grows and sustains life.
     """
-    def __init__(self, position, lifetime=0, energy=20 + randint(-5, 5), reproduction_threshold=30, energy_max=100):
+    def __init__(self, position, energy, reproduction_threshold, energy_max):
         """
         Food Initialisation
         :param position: The current position of the food in the world
-        :param lifetime: The lifetime of the food
         :param energy: The energy stored in the food
         :param reproduction_threshold: The energy value at which the food reproduces
         :param energy_max: The maximum energy the food can hold
         """
-        Organism.__init__(self, position, lifetime, energy, reproduction_threshold, energy_max)
+        Organism.__init__(self, position, energy, reproduction_threshold, energy_max)
 
     def grow(self):
         if self.energy < self.energy_max:
