1c749d45f39730b8930481e6d3f9e53ee1cf0946
Zack <zixux@hotmail.com>
2016-11-30 19:08:37 +0000

    Optimised for no 2D gene space

--- gene_viewer.py
+++ gene_viewer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import scipy.interpolate
+import evolution_switches as es
 from collections import OrderedDict
 from matplotlib import pyplot as plt
 
@@ -116,74 +117,75 @@ class GeneViewer:
             plt.title('time=%s' % i)
             plt.savefig(os.path.join('data', self.world.seed, 'bug_gene_data', '%s.png' % i))
             plt.close()
-        
-        # 2D plot(contours)
-        for i in range(self.world.time):
 
-            rep_thresh = self.split_list(food_gene_data['reproduction_threshold'])[i]
-            gene_val = self.split_list(food_gene_data['gene_val'])[i]
-            z_list = []
-            for value in zip(rep_thresh, gene_val):
-                z_list.append(value)
+        if es.gene_value:
+            # 2D plot(contours)
+            for i in range(self.world.time):
 
-            data = OrderedDict([(x, z_list.count(x)) for x in z_list])
-            old_z = [x for x in data.values()]
-            total = sum(old_z)
-            z = [x / total for x in old_z]
+                rep_thresh = self.split_list(food_gene_data['reproduction_threshold'])[i]
+                gene_val = self.split_list(food_gene_data['gene_val'])[i]
+                z_list = []
+                for value in zip(rep_thresh, gene_val):
+                    z_list.append(value)
 
-            trait_dict = OrderedDict.fromkeys(zip(rep_thresh, gene_val))
-            x = [d[0] for d in trait_dict]
-            y = [d[1] for d in trait_dict]
+                data = OrderedDict([(x, z_list.count(x)) for x in z_list])
+                old_z = [x for x in data.values()]
+                total = sum(old_z)
+                z = [x / total for x in old_z]
 
-            xi, yi = np.linspace(0, 101, 101), np.linspace(0, 359, 359)
-            xi, yi = np.meshgrid(xi, yi)
+                trait_dict = OrderedDict.fromkeys(zip(rep_thresh, gene_val))
+                x = [d[0] for d in trait_dict]
+                y = [d[1] for d in trait_dict]
 
-            if len(z) > 1:
-                rbf = scipy.interpolate.Rbf(x, y, z, function='linear')
-                zi = rbf(xi, yi)
-                plt.imshow(zi, vmin=min(z), vmax=max(z), origin='lower', aspect='auto')
+                xi, yi = np.linspace(0, 101, 101), np.linspace(0, 359, 359)
+                xi, yi = np.meshgrid(xi, yi)
 
-            plt.scatter(x, y, c=z)
-            plt.colorbar()
-            plt.xlim(0, 101)
-            plt.ylim(0, 359)
-            plt.xlabel('Reproduction Threshold')
-            plt.ylabel('Gene Value')
-            plt.title('time=%s' % i)
-            plt.savefig(os.path.join('data', self.world.seed, 'food_gene_space', '%s.png' % i))
-            plt.close()
+                if len(z) > 1:
+                    rbf = scipy.interpolate.Rbf(x, y, z, function='linear')
+                    zi = rbf(xi, yi)
+                    plt.imshow(zi, vmin=min(z), vmax=max(z), origin='lower', aspect='auto')
 
-        for i in range(self.world.time):
+                plt.scatter(x, y, c=z)
+                plt.colorbar()
+                plt.xlim(0, 101)
+                plt.ylim(0, 359)
+                plt.xlabel('Reproduction Threshold')
+                plt.ylabel('Gene Value')
+                plt.title('time=%s' % i)
+                plt.savefig(os.path.join('data', self.world.seed, 'food_gene_space', '%s.png' % i))
+                plt.close()
 
-            rep_thresh = self.split_list(bug_gene_data['reproduction_threshold'])[i]
-            gene_val = self.split_list(bug_gene_data['gene_val'])[i]
-            z_list = []
-            for value in zip(rep_thresh, gene_val):
-                z_list.append(value)
+            for i in range(self.world.time):
 
-            data = OrderedDict([(x, z_list.count(x)) for x in z_list])
-            old_z = [x for x in data.values()]
-            total = sum(old_z)
-            z = [x / total for x in old_z]
+                rep_thresh = self.split_list(bug_gene_data['reproduction_threshold'])[i]
+                gene_val = self.split_list(bug_gene_data['gene_val'])[i]
+                z_list = []
+                for value in zip(rep_thresh, gene_val):
+                    z_list.append(value)
 
-            trait_dict = OrderedDict.fromkeys(zip(rep_thresh, gene_val))
-            x = [d[0] for d in trait_dict]
-            y = [d[1] for d in trait_dict]
+                data = OrderedDict([(x, z_list.count(x)) for x in z_list])
+                old_z = [x for x in data.values()]
+                total = sum(old_z)
+                z = [x / total for x in old_z]
 
-            xi, yi = np.linspace(0, 101, 101), np.linspace(0, 359, 359)
-            xi, yi = np.meshgrid(xi, yi)
+                trait_dict = OrderedDict.fromkeys(zip(rep_thresh, gene_val))
+                x = [d[0] for d in trait_dict]
+                y = [d[1] for d in trait_dict]
 
-            if len(z) > 1:
-                rbf = scipy.interpolate.Rbf(x, y, z, function='linear')
-                zi = rbf(xi, yi)
-                plt.imshow(zi, vmin=min(z), vmax=max(z), origin='lower', aspect='auto')
+                xi, yi = np.linspace(0, 101, 101), np.linspace(0, 359, 359)
+                xi, yi = np.meshgrid(xi, yi)
 
-            plt.scatter(x, y, c=z)
-            plt.colorbar()
-            plt.xlim(0, 101)
-            plt.ylim(0, 359)
-            plt.xlabel('Reproduction Threshold')
-            plt.ylabel('Gene Value')
-            plt.title('time=%s' % i)
-            plt.savefig(os.path.join('data', self.world.seed, 'bug_gene_space', '%s.png' % i))
-            plt.close()
+                if len(z) > 1:
+                    rbf = scipy.interpolate.Rbf(x, y, z, function='linear')
+                    zi = rbf(xi, yi)
+                    plt.imshow(zi, vmin=min(z), vmax=max(z), origin='lower', aspect='auto')
+
+                plt.scatter(x, y, c=z)
+                plt.colorbar()
+                plt.xlim(0, 101)
+                plt.ylim(0, 359)
+                plt.xlabel('Reproduction Threshold')
+                plt.ylabel('Gene Value')
+                plt.title('time=%s' % i)
+                plt.savefig(os.path.join('data', self.world.seed, 'bug_gene_space', '%s.png' % i))
+                plt.close()
