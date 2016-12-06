9d1f5d634532a858a5179469837e318bc4a6399d
Zack <zixux@hotmail.com>
2016-12-06 18:35:28 +0000

    Fixed merge issues. Slight efficiency improvement

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from world_viewer import WorldViewer
 my_world = World(rows=80, columns=80, fertile_lands=[[[10, 10], [69, 69]]])
 # my_world = World(rows=3, columns=3)
 
+world_recorder = WorldRecorder(my_world)
 world_viewer = WorldViewer(my_world)
-gene_viewer = GeneViewer(my_world)
 
 my_world.spawn_food(200)
 my_world.spawn_bug(50)
@@ -76,18 +76,22 @@ while len(my_world.bug_list) > 0 and not _list:
                     bug.move(random_direction)
                     my_world.grid[tuple(bug.position)] += OrganismType.bug
 
-            # Check if bug can eat food
-            for j, food in enumerate(my_world.food_list):
-                if (bug.position == food.position).all():
-                    if bug.taste - 10 <= food.taste <= bug.taste + 10:
-                        bug.eat(food)
-                        my_world.kill(food)
-                    break
+            # Check if there is food on this square
+            if my_world.grid[tuple(bug.position)] == OrganismType.food_bug:
+                for j, food in enumerate(my_world.food_list):
+                    # Find the food
+                    if (bug.position == food.position).all():
+                        # Check if bug can eat it
+                        if bug.taste - 10 <= food.taste <= bug.taste + 10:
+                            bug.eat(food)
+                            my_world.kill(food)
+                        break
 
             # Check if bug can reproduce
             if bug.energy >= bug.reproduction_threshold:
                 random_direction = Direction.random(
                     my_world.get_disallowed_directions(bug.position, OrganismType.bug))
+                # Check if there is an empty square
                 if random_direction is not None:
                     new_bug = bug.reproduce(random_direction)
                     my_world.bug_list.append(new_bug)
