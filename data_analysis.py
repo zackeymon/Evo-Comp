6e6c6217d39f429107b4cf46a11b0b71788b4276
Zack <zixux@hotmail.com>
2017-01-11 13:33:40 +0000

    Added plant only analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from scipy.optimize import minimize, differential_evolution
+from scipy.optimize import minimize, differential_evolution, brute, basinhopping
+
+CC = 40000
 
 
 def lv(population, t, alpha, beta, delta, gamma):
     x, y = population
-    # print(population)
     dx_dt = x * (alpha - beta * y)
     dy_dt = y * (delta * x - gamma)
     return [dx_dt, dy_dt]
@@ -14,36 +15,43 @@ def lv(population, t, alpha, beta, delta, gamma):
 
 def competitive_lv(population, t, alpha, beta, delta, gamma):
     x, y = population
-    carrying_capacity = 2500
-    dx_dt = alpha * x * (1 - x / carrying_capacity) - beta * x * y
-    dy_dt = delta * x * y - gamma * y * (1 - y / carrying_capacity)
+    dx_dt = alpha * x * (1 - x / CC) - beta * x * y
+    dy_dt = delta * x * y - gamma * y
     return [dx_dt, dy_dt]
+
+
+def restricted_growth(x, t, alpha):
+    dx_dt = alpha * x * (1 - x / CC)
+    return dx_dt
 
 
 # TODO: Competitive LV
 # I think the objective function is right. We need better data sets
 
-def objective(parameters, data):
+def objective(parameters, *args):
+    func, data = args
     sample_size = len(data)
     t = np.arange(0, sample_size, 1)
-    lv_model = odeint(competitive_lv, data[0], t, args=tuple(parameters))
-    return np.sum(np.abs(lv_model - data))
-
+    model = np.array(odeint(func, data[0], t, args=tuple(parameters)))
+    if model.shape[1] == 1:
+        model = model.ravel()
+    return np.sum(np.abs(model - data))
 
 if __name__ == '__main__':
-    guess_parameters = [0.08, 0.0002, 0.001, 0.01]
-    parameters_bounds = ((0.0000001, 0.2), (0.0000001, 0.2), (0.0000001, 0.2), (0.0000001, 0.2))
+    guess_parameters = [0.008, 0.00002, 0.001, 0.001]
+    parameters_bounds = ((1e-7, 0.5), (1e-7, 0.1), (1e-7, 0.1), (1e-7, 0.5))
     # TODO: Constraint dictionary
 
-    data = np.loadtxt('population_data.csv', delimiter=',')
+    data = np.loadtxt('pop_data_.csv', delimiter=',')  # [plants, bugs]
     t = np.arange(0, len(data), 1)
 
     plt.plot(t, data[:, 0], t, data[:, 1])
-    opt_parameters = differential_evolution(objective, parameters_bounds, args=(data,))
-    # opt_parameters = minimize(objective, guess_parameters, args=(data,), bounds=parameters_bounds)
+    # opt_parameters = brute(objective, parameters_bounds, args=(data,))
+    opt_parameters = minimize(objective, guess_parameters, args=(lv, data), bounds=parameters_bounds)
+    # opt_parameters = basinhopping(objective, guess_parameters, niter=1000, minimizer_kwargs={'args': (data,)})
     print(opt_parameters)
 
-    sol = odeint(competitive_lv, data[0], t, args=tuple(opt_parameters.x))
+    sol = odeint(lv, data[0], t, args=tuple(opt_parameters.x))
     plt.plot(t, sol[:, 0], 'black')
     plt.plot(t, sol[:, 1], 'red')
     plt.show()
---
+++ data_plants_only.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy.optimize import minimize
+from data_analysis import objective, restricted_growth
+
+data = np.loadtxt('plant_rt30_gr2.csv')
+t = np.arange(0, len(data), 1)
+
+plt.plot(t, data, '.-')
+
+guess = [0.01]
+bounds = ((0, 0.5),)
+
+# 2D minimisation??
+
+opt = minimize(objective, guess, args=(restricted_growth, data), bounds=bounds)
+print(opt)
+sol = odeint(restricted_growth, data[0], t, args=tuple(opt.x))
+plt.plot(t, sol, '.-')
+plt.show()
