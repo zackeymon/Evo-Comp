0a407395defd29d60cec42c024783465fbc383fa
Anthony Li <ant385@hotmail.co.uk>
2017-01-15 16:22:16 +0000

    Try this

--- main_data_plotter.py
+++ main_data_plotter.py
@@ -4,7 +4,7 @@ Copy config.py file from (seeded) folder of world to view.
 
 from world_viewer import WorldViewer
 
-world_viewer = WorldViewer('rt0_t0-128-gr1-01')  # seed of world to view
+world_viewer = WorldViewer('rt0_t0-128-gr1')  # seed of world to view
 
 world_viewer.plot_world_stats()
-world_viewer.plot_world_data(world=True, start=150)
+world_viewer.plot_world_data(day=100, world=True)
